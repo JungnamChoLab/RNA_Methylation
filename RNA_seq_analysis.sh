e1182a30d0e73f11ad8b45dbfa9b33c52ae17bf3
JungnamChoLab <67249636+JungnamChoLab@users.noreply.github.com>
2022-08-30 12:57:54 +0800

    Update RNA_seq_analysis.sh

--- RNA_seq_analysis.sh
+++ RNA_seq_analysis.sh
@@ -1,4 +1,4 @@
-###RNA-seq SG-RNA-seq follows the same process
+###All RNA-seq and SG-RNA-seq follows the same process
 mkdir 0_fastqc 3_trim 4_hisat2 5_stringtie 6_featurecounts
 cd 2.cleandata;ls | sed /name/d > name
 cp name ../3_trim;cp name ../4_hisat2;cp name ../5_stringtie;
@@ -45,3 +45,43 @@ ls */*.gtf > sample_lst.txt ### edit sample_lst.txt ###%s#\(\S\+\)\/#\1 \1/#
 python prepDE.py -i sample_lst.txt
 
 ###gene_count_matrix.csv was used for DESeq2 DEG analysis
+g <- read.csv("gene_count_matrix.csv",row.names = "gene_id")
+colnames(g)
+g <- g[,c(1:6,10:12,7:9)]
+colnames(g)
+colnames(g)<- c("Col-0 rep1","Col-0 rep2","Col-0 rep3","SG Col-0 rep1","SG Col-0 rep2","SG Col-0 rep3","alkbh9b-1 rep1",
+"alkbh9b-1 rep2","alkbh9b-1 rep3","SG alkbh9b-1 rep1","SG alkbh9b-1 rep2","SG alkbh9b-1 rep3")
+countData <- as.matrix(g)
+library("DESeq2")
+library("ggplot2")
+#rep(c(1,2),3,each=3)
+colData <- data.frame("genotype"=rep(c("WT","Mutant"),1,each=6),
+                      "condition"=rep(c("Total","SG"),2,each=3))
+colData
+rownames(colData) <- colnames(countData)
+colData
+all(rownames(colData) %in% colnames(countData))
+dds <- DESeqDataSetFromMatrix(countData = countData,
+                              colData = colData,
+                              design = ~genotype+condition)
+dds$genotype
+dds$condition
+dds$genotype <- factor(dds$genotype, levels = c("WT","Mutant"))
+dds$condition <- factor(dds$condition, levels = c("Total","SG"))
+dds$genotype
+dds$condition
+dds <- DESeq(dds)
+#Drawing sample matrix and PCA to check replicates
+rld <- rlog(dds)
+vsd <- vst(dds)
+sampleDists <- dist( t( assay(rld) ) )
+#sampleDists <- dist( t( assay(vsd) ) )
+sampleDistMatrix <- as.matrix( sampleDists )
+library( pheatmap )
+library( "RColorBrewer" )
+colours = colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
+pheatmap( sampleDistMatrix,
+          clustering_distance_rows = sampleDists,
+          clustering_distance_cols = sampleDists,
+          col = colours, border_color = NA )
+plotPCA(rld,intgroup=c("condition","genotype"))
